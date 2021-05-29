dfd843ea3fa6b009fac462db76219ae56056d1f9
デンパ時計 <jsoldenpa@yahoo.co.jp>
2021-05-29 20:51:31 +0900

    プロジェクト ファイルを追加します。

---
+++ OculusEnableHighFPS/Patches/VRRenderingParamsSetupOnEnablePatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OculusEnableHighFPS.Patches
+{
+    [HarmonyPatch(typeof(VRRenderingParamsSetup), "OnEnable")]
+    public class VRRenderingParamsSetupOnEnablePatch
+    {
+        public static void Postfix()
+        {
+            OVRPlugin.systemDisplayFrequency = 120f;
+        }
+    }
+}
